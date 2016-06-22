f4ea12d95ddfb85ab480528c6db37f881b7c3e27
Deepak Jois <deepak.jois@gmail.com>
2016-06-22 12:40:36 +0530

    Script to calculate size of courses

---
+++ download-courses/calculate_size.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+ aws s3 ls s3://moocfetcher-course-archive --recursive  | grep -v -E "(Bucket: |Prefix: |LastWriteTime|^$|--)" | awk 'BEGIN {total=0}{total+=$3}END{print total/1024/1024" MB"}'
