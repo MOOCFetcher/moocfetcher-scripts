389243df2ec1657683218ee6d533a66623809107
Deepak Jois <deepak.jois@gmail.com>
2016-07-13 18:50:17 +0530

    Fold English course script into main download info script

--- download-courses/list_english_courses.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-cat launched.json | jq -r  '.courses[] | select(.primaryLanguageCodes) | select(.primaryLanguageCodes | map(. == "en") | any) |  .slug' | sort
--- download-courses/update_downloaded.sh
+++ download-courses/update_downloaded.sh
@@ -4,5 +4,8 @@ cat launched.json | jq -r ".courses[] |  .slug" | sort > launched.txt
 echo "`wc -l < launched.txt` courses launched"
 aws s3 ls s3://moocfetcher-course-archive/coursera/ | grep PRE | awk '{print $2}' | tr -d '/' | sort > downloaded.txt
 echo "`wc -l < downloaded.txt` courses downloaded"
+cat launched.json | jq -r  '{courses: [.courses[] | select(.primaryLanguageCodes) | select(.primaryLanguageCodes | map(. == "en") | any)]}' > english.json
+echo "`cat english.json | jq '.courses[] | .slug' | wc -l` courses in English"
 comm -23 launched.txt downloaded.txt > missing.txt
 echo "`wc -l < missing.txt` courses missing"
+
