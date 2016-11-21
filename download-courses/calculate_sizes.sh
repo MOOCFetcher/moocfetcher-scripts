7b6bd707cf7384cbbb060edf194eef9ee70bac39
Deepak Jois <deepak.jois@gmail.com>
2016-11-21 13:11:29 +0530

    Updated courses

--- download-courses/calculate_sizes.sh
+++ download-courses/calculate_sizes.sh
@@ -4,5 +4,4 @@ coursesSize() {
   cat $1 | jq  ".courses | map(.size) | add  | ./(1024*1024*1024) | floor"
 }
 
-echo "All courses: `coursesSize launched.json`GB"
 echo "English courses: `coursesSize english.json`GB"
--- download-courses/update_downloaded.sh
+++ download-courses/update_downloaded.sh
@@ -3,11 +3,11 @@ aws s3 cp --quiet s3://moocfetcher/coursera/all.json .
 echo "`cat all.json | jq '.courses | map(select(.courseType == "v2.ondemand")) | .[] | .slug ' | wc -l` courses total"
 aws s3 cp --quiet s3://moocfetcher/coursera/ondemand/launched.json .
 echo "`cat launched.json | jq '.courses[] | .slug' | wc -l` courses launched"
-aws s3 ls s3://moocfetcher-course-archive/coursera/ | grep PRE | awk '{print $2}' | tr -d '/' | sort > downloaded.txt
-echo "`wc -l < downloaded.txt` courses downloaded"
 cat launched.json | jq -r  '{courses: [.courses[] | select(.primaryLanguageCodes) | select(.primaryLanguageCodes | map(. == "en") | any)]}' > english.json
 echo "`cat english.json | jq '.courses[] | .slug' | wc -l` courses in English"
 cat english.json | jq -r ".courses[] |  .slug" | sort > english.txt
+aws s3 ls s3://moocfetcher-course-archive/coursera/ | grep PRE | awk '{print $2}' | tr -d '/' | sort > downloaded.txt
+echo "`wc -l < downloaded.txt` courses in English downloaded"
 comm -23 english.txt downloaded.txt > missing.txt
 echo "`wc -l < missing.txt` courses in English missing"
 
