f3c55f02b02002f8e41e6b1faa63d1d083ddf802
Deepak Jois <deepak.jois@gmail.com>
2017-07-30 23:11:01 +0530

    Use variable for course volume

--- download/post-process.sh
+++ download/post-process.sh
@@ -4,7 +4,7 @@
 moocfetcher us
 
 # Filter downloaded courses
-moocfetcher fc -d /Volumes/courses
+moocfetcher fc -d $COURSE_VOLUME
 
 # Update annotated CSV file
 csvjoin --delimiter "," --quotechar "\"" --outer -c "Folder on Disk" courses.csv courses-annotated.csv | csvcut  --delimiter "," --quotechar "\"" -c 1,2,3,7,8,9 | csvsort --delimiter "," --quotechar "\"" -c 4,1 | sponge courses-annotated.csv
