d597eb0022fc8f35e36ebb4661eed731dadbe75d
Deepak Jois <deepak.jois@gmail.com>
2018-01-16 20:08:55 +0530

    Tweak post-process script to invoke fillinfo.go

--- download/post-process.sh
+++ download/post-process.sh
@@ -9,5 +9,11 @@ moocfetcher fc -d $COURSE_VOLUME
 # Update annotated CSV file
 csvjoin --delimiter "," --quotechar "\"" --outer -c "Folder on Disk" courses.csv courses-annotated.csv | csvcut  --delimiter "," --quotechar "\"" -c 1,2,3,7,8,9 | csvsort --delimiter "," --quotechar "\"" -c 4,1 | sponge courses-annotated.csv
 
+# Take a backup
+cp courses-annotated.csv courses-annotated.csv.bak
+
+# Update data from site
+go run fillinfo.go | sponge courses-annotated.csv
+
 # Generate excel file
 python csv2excel.py
